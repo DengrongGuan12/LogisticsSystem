4bc5f861a8f90702af1d1c254f06b3bc31945792
dengrongguan <1031212913@qq.com>
2017-11-20 23:38:29 +0800

    init

---
+++ src/main/java/Hello.java
@@ -0,0 +1,23 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by dengrongguan on 2017/11/20.
+ */
+@Controller
+@EnableAutoConfiguration
+public class Hello {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Hello.class, args);
+    }
+}
